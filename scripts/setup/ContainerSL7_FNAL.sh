5dba2ac0947b88ce1bc30e898ef8a63b4482afdb
CrossR <ryan.cross@warwick.ac.uk>
2026-08-20 09:52:31 +0100

    Update SL7 container code.

--- scripts/setup/ContainerSL7_FNAL.sh
+++ scripts/setup/ContainerSL7_FNAL.sh
@@ -1,1 +1,7 @@
-apptainer shell --env PS1="C@\h:\W$ " --shell=/bin/bash -B /cvmfs,/exp,/nashome,/pnfs/dune,/opt,/run/user,/etc/hostname,/etc/hosts,/etc/krb5.conf /cvmfs/singularity.opensciencegrid.org/fermilab/fnal-dev-sl7:latest
+BIND_PATHS="/cvmfs,/exp,/nashome,/opt,/run/user,/etc/hostname,/etc/hosts,/etc/krb5.conf"
+
+if [ -d "/pnfs/dune" ]; then
+    BIND_PATHS="${BIND_PATHS},/pnfs/dune"
+fi
+
+apptainer shell --env PS1="C@\h:\W$ " --shell=/bin/bash -B ${BIND_PATHS} /cvmfs/singularity.opensciencegrid.org/fermilab/fnal-dev-sl7:latest
